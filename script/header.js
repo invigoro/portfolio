52c4f0e91fb9ffc28173dfc0931a2c9ac640181c
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2026-01-02 17:46:54 -0700

    updates

---
+++ script/header.js
@@ -0,0 +1,23 @@
+const navbar = `<nav class="navbar navbar-expand-sm bg-dark navbar-purple justify-content-center">
+			<a class="navbar-brand" href="/index.html">Timothy Wells</a>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="/html/coding.html">Coding Projects</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/html/other.html">Video Games</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/html/films.html">Films</a>
+				</li>
+				<li class='nav-item'>
+					<a class='nav-link' href="/files/Wells_CV.pdf" download>Download Résumé</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/html/contact.html">Contact</a>
+				</li>
+			</ul>
+		</nav>`;
+document.getElementById("navbar").innerHTML = navbar;
+document.getElementById("navbar").classList.add("sticky-top");
+scrollTo(0, 0);
